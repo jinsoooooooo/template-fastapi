255d9af63413991237d88b318f26a02bc7e9f49c
jinsoo.lim <ljslim27@gmail.com>
2026-02-23 17:28:16 +0900

    refactor(middelware): add loggin middleware for default

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from base.middleware import LoggingMiddleware
 
-app = FastAPI()
+
+
+@asynccontextmanager    
+async def lifespan(app: FastAPI):
+    yield
+
+app = FastAPI(
+    title="Sample FastAPI",
+    description="Sample FastAPI",
+    version="0.1.0",
+    lifespan=lifespan
+)
+
+app.add_middleware(LoggingMiddleware)
 
 @app.get("/healthz")
 def healthz():
@@ -8,4 +23,13 @@ def healthz():
 
 @app.get("/api/hello")
 def hello():
-    return {"message": "hello from fastapi on home k8s"}
+    return {"message": "hello from fastapi"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        app, 
+        host="0.0.0.0", 
+        port=8001,
+        log_level="debug"
+    )
